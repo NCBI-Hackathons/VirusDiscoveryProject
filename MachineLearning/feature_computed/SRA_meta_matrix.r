911f7a7855b0f4741fd166cc846988b0c8a38f08
bkellman <bkellman@eng.ucsd.edu>
2019-01-11 21:34:26 +0000

    sra metadata

--- MachineLearning/feature_computed/SRA_meta_matrix.r
+++ MachineLearning/feature_computed/SRA_meta_matrix.r
@@ -1,3 +1,4 @@
+library(stringr)
 library(reshape2)
 
 #
@@ -6,9 +7,10 @@ setwd('~/VirusDiscoveryProject')
 meta_sel = read.csv("MachineLearning/data_in/SRAdb/SRA.sel_SRR.csv")
 
 # word vector
-words = strsplit(gsub('NA','',apply(meta_sel[,4:5],1,paste,collapse=' ')),' ')
+words = strsplit(gsub('NA','',apply(meta_sel[,4:5],1,paste,collapse=' ')),"\\s+")
 names(words) = as.character( meta_sel$run_accession ) 
 word_melt = melt(words)
+word_melt$value = str_replace_all(as.character(word_melt$value), "[[:punct:]]", " ")
 
 # out 
 tab = table(word_melt)
