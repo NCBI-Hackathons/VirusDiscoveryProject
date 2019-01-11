33751723cf97d848e84454c1e2a85f4945c70b25
bkellman <bkellman@eng.ucsd.edu>
2019-01-11 19:06:45 +0000

    rerun sra query

--- MachineLearning/data_in/SRAdb/sra_query.r
+++ MachineLearning/data_in/SRAdb/sra_query.r
@@ -17,13 +17,22 @@ srr = as.character(read.table('../allSRR.txt')[[1]])
 # query
 i=0
 #rs <- dbGetQuery(sra_con,'select study_abstract from study where study_accession is "SRR2992957"')
+
+# selected query
 rs=do.call(rbind,lapply(srr,function(x){
- #i=i+1
  print(x)
- #print(paste(i,'/',length(srr)))
  dbGetQuery(sra_con,paste0("select run.run_accession,run.submission_accession,study.study_abstract,study.study_description from study INNER JOIN run ON study.submission_accession=run.submission_accession where run.run_accession is '",
 	x,"'")) 
 }))
 
 write.csv(rs,file='SRA.sel_SRR.csv')
+
+# all query
+rs=do.call(rbind,lapply(srr,function(x){
+ print(x)
+ dbGetQuery(sra_con,paste0("select * from study INNER JOIN run ON study.submission_accession=run.submission_accession where run.run_accession is '",
+	x,"'")) 
+}))
+
+write.csv(rs,file='SRA.all_SRR.csv')
 
