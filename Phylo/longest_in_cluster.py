e3cd5883b6aceaeb98fc384ed54140abe47ab465
kylelevi <kylemlevi@gmail.com>
2019-01-10 18:31:12 -0800

    can now extract the longest seqs from each cluster to a new fasta

--- Phylo/longest_in_cluster.py
+++ Phylo/longest_in_cluster.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--fasta', help='Input File', required=True)
     parser.add_argument('-c', '--clusters', help='Input File', required=True)
     parser.add_argument('-o', '--output', help='Input File', required=True)
+    parser.add_argument('-e', '--extractlongest', help='instead of remaking the clusters around the longest, extract the longest fasta to a new file')
 
 
     try:
@@ -23,7 +24,7 @@ if __name__ == '__main__':
 
 
     # fasta_file = 'example_files/test_contigs.fasta'
-    # cluster_file = 'example_files/new_clusters.tsv'
+    # cluster_file = 'example_files/test_clusters.tsv'
     # outf = 'example_files/exampleout.tsv'
     fasta_file = args.fasta
     cluster_file = args.cluster
@@ -51,7 +52,7 @@ if __name__ == '__main__':
         for line in infile:
             line = line.replace('\n', '').split('\t')
             clusters[line[0]] = clusters.get(line[0], []) + [line[1]]
-    print(clusters)
+
 
     # define new clusters
     newclusters = {}
@@ -59,13 +60,27 @@ if __name__ == '__main__':
         sizes = [genome_lens[x] for x in v]
         biggest = v[sizes.index(max(sizes))]
         newclusters[biggest] = v
+    if not args.extractlongest:
+        # write new file
+        with open(outf, 'w') as outfile:
+            for k,v in newclusters.items():
+                for child in v:
+                    outfile.write(k + '\t' + child + '\n')
 
-    # write new file
-    with open(outf, 'w') as outfile:
-        for k,v in newclusters.items():
-            for child in v:
-                outfile.write(k + '\t' + child + '\n')
-
-
+    else:
+        longests = set(newclusters.keys())
+        with open(outf, 'w') as outfile:
+            with open(fasta_file, 'r') as infile:
+                keeper = False
+                for line in infile:
+                    if line.startswith('>'):
+                        if line[1:].strip() in longests:
+                            keeper = True
+                            outfile.write(line)
+                        else:
+                            keeper = False
+                    else:
+                        if keeper:
+                            outfile.write(line)
 
 
