f406ba74155bda7d2dd76de34537db06f4b1c30f
joanmarticarreras <joan.marti.carreras@gmail.com>
2019-01-12 01:34:53 +0100

    director.sh v3 + scripts/unmapvigaannotations3.pl

--- VirusGenes/scripts/director.sh
+++ VirusGenes/scripts/director.sh
@@ -4,7 +4,6 @@
 # This script directs the usage of VIGA by making use of parallel
 # It parallelises all different fasta files with target contigs and runs VIGA pipeline single threaded for each one of them
 
-
 parallel '      name=$(basename {} | sed "s/\.fasta//g")
 		mkdir result/${name}
 		cp run-viga result/${name}
@@ -12,11 +11,10 @@ parallel '      name=$(basename {} | sed "s/\.fasta//g")
 		cp ${name}.fasta result/${name}
 		cd result/${name}
 		./run-viga --input ${name}.fasta --diamonddb /data/databases/RefSeq_Viral_DIAMOND/refseq_viral_proteins.dmnd --blastdb /data/databases/RefSeq_Viral_BLAST/refseq_viral_proteins --rfamdb /data/databases/rfam/Rfam.cm --hmmerdb /data/databases/pvogs/pvogs.hmm --threads 1 --modifiers modifiers.txt
-		perl <working-dir>unmapvigaannotations.pl ${name}_annotated.csv ${name}.fasta ${name}_annotated_rename.csv
-		python <working-dir>genbankfeature.py ${name}_annotated.gbk
-		perl <working-dir>unmapvigaannotations2.pl ${name}_annotated.protein.faa ${name}.fasta ${name}_annotated_rename.protein.faa
-		python <working-dir>addvq.py ${name}_annotated_rename.csv ViralQuotient.txt ${name}_annotated_rename_vq.csv
-		python <working-dir>converter.py -i ${name}_annotated_rename_vq.csv -o ${name}_annotated_rename_vq.json
+		python /home/tully.bj/genbankfeature.py ${name}_annotated.gbk
+		perl /home/tully.bj/unmapvigaannotations3.pl ${name}.fasta ${name}_annotated.fasta ${name}_annotated.csv ${name}_annotated_rename.csv ${name}_annotated.protein.faa ${name}_annotated.protein_rename.faa
+		python /home/tully.bj/addvq.py ${name}_annotated_rename.csv ViralQuotient.txt ${name}_annotated_rename_vq.csv
+		python /home/tully.bj/converter.py -i ${name}_annotated_rename_vq.csv -o ${name}_annotated_rename_vq.json
 
 
 ' ::: $(ls *.fasta)
