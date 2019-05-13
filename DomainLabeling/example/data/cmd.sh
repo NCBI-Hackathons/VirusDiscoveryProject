ef82a57e61b19a86f14a554dace8776eafa14533
Warddeb <ward.deboutte@kuleuven.be>
2019-05-13 13:57:31 +0200

    tutorial contd

---
+++ DomainLabeling/example/data/cmd.sh
@@ -0,0 +1,36 @@
+#/bin/bash
+rpstblastn -db ./cdd_db/xaa -query line -evalue 1e-3 -out xaa.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xab -query line -evalue 1e-3 -out xab.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xac -query line -evalue 1e-3 -out xac.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xad -query line -evalue 1e-3 -out xad.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xae -query line -evalue 1e-3 -out xae.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xaf -query line -evalue 1e-3 -out xaf.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xag -query line -evalue 1e-3 -out xag.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xah -query line -evalue 1e-3 -out xah.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xai -query line -evalue 1e-3 -out xai.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xaj -query line -evalue 1e-3 -out xaj.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xak -query line -evalue 1e-3 -out xak.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xal -query line -evalue 1e-3 -out xal.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xam -query line -evalue 1e-3 -out xam.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xan -query line -evalue 1e-3 -out xan.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xao -query line -evalue 1e-3 -out xao.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xap -query line -evalue 1e-3 -out xap.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xaq -query line -evalue 1e-3 -out xaq.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xar -query line -evalue 1e-3 -out xar.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xas -query line -evalue 1e-3 -out xas.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xat -query line -evalue 1e-3 -out xat.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xau -query line -evalue 1e-3 -out xau.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xav -query line -evalue 1e-3 -out xav.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xaw -query line -evalue 1e-3 -out xaw.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xax -query line -evalue 1e-3 -out xax.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xay -query line -evalue 1e-3 -out xay.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xaz -query line -evalue 1e-3 -out xaz.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xba -query line -evalue 1e-3 -out xba.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xbb -query line -evalue 1e-3 -out xbb.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xbc -query line -evalue 1e-3 -out xbc.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xbd -query line -evalue 1e-3 -out xbd.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xbe -query line -evalue 1e-3 -out xbe.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xbf -query line -evalue 1e-3 -out xbf.line.rpstbln -outfmt 15 &
+rpstblastn -db ./cdd_db/xbg -query line -evalue 1e-3 -out xbg.line.rpstbln -outfmt 15 &
+wait
+
